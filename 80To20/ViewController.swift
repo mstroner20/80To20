18426c0be677a3f1ef93ce4c234e13c0034048b3
Michael Stroner <mstroner566@gmail.com>
2020-07-19 13:06:32 -0500

    Initial Commit

---
+++ 80To20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  80To20
+//
+//  Created by Mike Stroner on 7/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 80To20Tests/_0To20Tests.swift
@@ -0,0 +1,33 @@
+//
+//  _0To20Tests.swift
+//  80To20Tests
+//
+//  Created by Mike Stroner on 7/19/20.
+//
+
+import XCTest
+@testable import _0To20
+
+class _0To20Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
