bda75dbad017a86376515029032da960350d9c2b
Michael Stroner <mstroner566@gmail.com>
2021-05-21 14:34:00 -0500

    -Initial push

--- 80To20/ViewController.swift
+++ 80To20/ViewController.swift
@@ -7,18 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate{
 
     @IBOutlet weak var batteryLabel: UILabel?;
+    @IBOutlet var messageLabel: UILabel!;
     var batteryValue: Int = 0;
     
     private var observer : NSObjectProtocol?;
+    
+    let userNotificationCenter = UNUserNotificationCenter.current();
    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.userNotificationCenter.delegate = self;
+        
         checkBattery(batteryLevel: batteryValue);
         changeText();
+        displayMessage(currentLevel: batteryValue);
+        requestNotificationAuth();
+        sendNotification();
 
         observer = NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main){
             [unowned self] notification in
@@ -38,11 +46,63 @@ class ViewController: UIViewController {
         batteryLabel!.font = UIFont(name:"Arial", size: 45);
     }
     
-    @IBAction func lowPowerSwitchonChange(_ sender: Any) {
+    func requestNotificationAuth(){
+        let authOptions = UNAuthorizationOptions.init(arrayLiteral: .alert, .badge, .sound);
+        self.userNotificationCenter.requestAuthorization(options: authOptions) {(success, error) in
+            if let error = error{
+                print("Error: ", error);
+            }
+            
+        }
+    }
+    
+    
+    func sendNotification()
+    {
+        let notificationContent = UNMutableNotificationContent();
+        notificationContent.title = "Battery Level";
+        notificationContent.body = "\(batteryValue)% ";
+        notificationContent.badge = NSNumber(value:0);
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: false);
+        let request = UNNotificationRequest(identifier: "testNotification", content: notificationContent, trigger: trigger);
         
         
+        userNotificationCenter.add(request) {(error) in
+            if let error = error{
+                print("Notification Error: ", error);
+            }
+        }
         
     }
+    
+    func displayMessage(currentLevel:Int)
+    {
+        messageLabel.textAlignment = .center;
+        messageLabel.center.x = self.view.center.x;
+        messageLabel!.font = UIFont(name:"Arial", size: 23.5);
+        
+        if(currentLevel == 80)
+        {
+            messageLabel.text = "Perfectly charged. Good job!";
+        }
+        else if(currentLevel >= 50 && currentLevel < 80)
+        {
+            messageLabel.text = "In a good place.";
+        }
+        else if(currentLevel < 50 && currentLevel > 20)
+        {
+            messageLabel.text = "Quickly approaching 20";
+            messageLabel.center.x = self.view.center.x;
+            
+        }
+        else{
+            messageLabel.text = "Mission Critical. Please plug in.";
+        }
+        
+       
+    }
+    
     
     
    
