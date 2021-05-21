4be789f539663f2ddfac0a6d98b5a644e6012d4e
Michael Stroner <mstroner566@gmail.com>
2021-05-21 07:35:02 -0500

    Testing to see if this still works

--- 80To20/ViewController.swift
+++ 80To20/ViewController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var batteryLabel: UILabel?;
+    var batteryValue: Int = 0;
+    
+    private var observer : NSObjectProtocol?;
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        checkBattery(batteryLevel: batteryValue);
+        changeText();
+
+        observer = NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main){
+            [unowned self] notification in
+            checkBattery(batteryLevel: batteryValue);
+        }
     }
+    
+    func checkBattery(batteryLevel : Int){
+        UIDevice.current.isBatteryMonitoringEnabled = true;
+        batteryValue = (Int)(UIDevice.current.batteryLevel * 100);
+        batteryLabel!.text = "\(batteryValue)%";
+    }
+    
+    func changeText(){
+        batteryLabel!.center = self.view.center;
+        batteryLabel!.textAlignment = .center;
+        batteryLabel!.font = UIFont(name:"Arial", size: 45);
+    }
+    
+    @IBAction func lowPowerSwitchonChange(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+   
+    
 
 
 }
